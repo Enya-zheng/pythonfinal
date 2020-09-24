16e48cd56e2575ce0382e753e33605bf84944bb2
Enya－zheng <49051488+1393150093@qq.com>
2020-09-24 23:02:30 +0800

    修改

--- IVfinal/final.py
+++ IVfinal/final.py
@@ -6,18 +6,18 @@ import plotly.graph_objs as go
 
 app = Flask(__name__)
 
-# 准备工作 什麽都没做
+# 准备工作
 df = pd.read_csv('NCHS_-_Potentially_Excess_Deaths_from_the_Five_Leading_Causes_of_Death.csv')
 States_available_loaded = list(df.State.dropna().unique())
 
-# 基本cufflinks 及ploty設置, 查文檔看書貼上而已
+# 基本cufflinks 及ploty設置
 cf.set_config_file(offline=True, theme="ggplot")
 py.offline.init_notebook_mode()
 
 @app.route('/',methods=['GET'])
 def IV_final_2019():
     data_str = df.to_html()
-    States_available = States_available_loaded #下拉选单还没内容
+    States_available = States_available_loaded #下拉选单
     return render_template('results2.html',
                            the_res = data_str,
                            the_select_State=States_available)
@@ -29,7 +29,7 @@ def IV_final_select() -> 'html':
 
     dfs = df.query("State=='{}'".format(the_State)) ## 使用df.query()方法. 按用户交互输入the_region过滤
 
-    data_str = dfs.to_html()  # <------------------数据产出dfs, 完成互动过滤呢
+    data_str = dfs.to_html()  # <------------------数据产出dfs, 完成互动过滤
     
     fig = dfs.iplot(kind="bar", x="Cause of Death", y="Year", asFigure=True)  # 使用iplot 做bar圖
     py.offline.plot(fig, filename="成果.html",auto_open=False)                  # 備出"成果.html"檔案之交互圖
